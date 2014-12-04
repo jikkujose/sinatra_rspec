7c3f7b7edbac53575e9db590a3a75d325f5661d8
Jikku Jose <jikkujose@gmail.com>
2014-12-05 00:06:45 +0530

    Refine app for better feedback

--- app/todo_api.rb
+++ app/todo_api.rb
@@ -1,10 +1,11 @@
 class TodoAPI < Sinatra::Base
   get '/' do
-    $db.join('\n')
+    $db.join("\n")
   end
 
   post '/' do
     halt 400 unless params[:todo]
     $db << params[:todo]
+    "Todo #{params[:todo]} saved!"
   end
 end
